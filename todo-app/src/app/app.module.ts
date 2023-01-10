39b3cf09c4fe1c8c74f71cd714cdac0dcf97ca89
kareemyasser8 <kareem.yasserr@gmail.com>
2023-01-10 22:10:03 +0200

    adding the task imput field

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoryDisplayerComponent } from './category-displayer/category-displayer.component';
+import { TaskInputComponent } from './task-input/task-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryDisplayerComponent
+    CategoryDisplayerComponent,
+    TaskInputComponent
   ],
   imports: [
     BrowserModule,
