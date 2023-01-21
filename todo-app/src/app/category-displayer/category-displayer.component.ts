009536bffa1f9bdd48a80168df83262071a44911
kareemyasser8 <kareem.yasserr@gmail.com>
2023-01-21 11:57:10 +0200

    update Category Name

--- todo-app/src/app/category-displayer/category-displayer.component.ts
+++ todo-app/src/app/category-displayer/category-displayer.component.ts
@@ -1,4 +1,6 @@
+import { Event, NavigationEnd, Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'category-displayer',
@@ -7,6 +9,7 @@ import { Component } from '@angular/core';
 })
 export class CategoryDisplayerComponent {
 
+  currentCategory: string;
   date: any = new Date();
   days: Array<string> = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   months: Array<string> =
@@ -23,7 +26,16 @@ export class CategoryDisplayerComponent {
       "November",
       "December"]
 
-  constructor() {
+  constructor(private router: Router) {
+    router.events.pipe(filter((event:Event) => event instanceof NavigationEnd))
+      .subscribe((event:any) => {
+        this.setCategoryType(event.url);
+      })
+  }
+
+  setCategoryType(url:string){
+      if(url == "/my-day") this.currentCategory = "My Day";
+      if(url == "/favourite-list") this.currentCategory = "Important";
 
   }
 
